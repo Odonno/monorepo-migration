39fcc1e39556845b7578aa591d884a2f7f244e34
Odonno <bottiau.david@laposte.net>
2023-02-16 21:15:14 +0100

    📝 mock fetch requests in game page story

--- apps/docs/stories/pages/Game.stories.tsx
+++ apps/docs/stories/pages/Game.stories.tsx
@@ -2,11 +2,42 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 import GamePage from "home/pages/game";
 
+const COLUMNS = [0, 1, 2];
+const ROWS = [0, 1, 2];
+
+const response = {
+  grid: ROWS.flatMap((y) => {
+    return COLUMNS.map((x) => {
+      return { x, y };
+    });
+  }),
+};
+
 export default {
   title: "Web/Pages",
   component: GamePage,
   parameters: {
     layout: "fullscreen",
+    mockData: [
+      {
+        url: "/api/game",
+        method: "GET",
+        status: 200,
+        response,
+      },
+      {
+        url: "/api/game",
+        method: "POST",
+        status: 200,
+        response,
+      },
+      {
+        url: "/api/game",
+        method: "DELETE",
+        status: 200,
+        response,
+      },
+    ],
   },
 } as ComponentMeta<typeof GamePage>;
 
